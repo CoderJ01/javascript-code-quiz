18a52baaad4b01e364fce2a583a0eddd0ca87373
Coder J <buildingguy@sbcglobal.net>
2021-12-05 15:13:36 -0800

    store references

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var startButton = document.getElementById('start');
+var containQuiz = document.getElementById('quiz');
+var containResults = document.getElementById('results');
+var submitButton = document.getElementById('submit');
