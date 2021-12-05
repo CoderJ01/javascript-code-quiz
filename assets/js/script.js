2baf1ca088b62b247b9aaabaca54e1f8d8acb6da
Coder J <buildingguy@sbcglobal.net>
2021-12-05 15:18:34 -0800

    create functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ var startButton = document.getElementById('start');
 var containQuiz = document.getElementById('quiz');
 var containResults = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+
+function quizStart () {
+
+}
+
+function displayResults () {
+
+}
+
+startQuiz();
+
+startButton.addEventListener('click', quizStart);
+
+submitButton.addEventListener('click', displayResults);
