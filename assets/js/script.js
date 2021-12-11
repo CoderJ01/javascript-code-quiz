56b2838a50d7f00881ecf8acdfd1cc883b482aff
Coder J <buildingguy@sbcglobal.net>
2021-12-10 17:44:53 -0800

    change choices from object {} to array []

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,276 +2,113 @@ var testQuestions = [
     {
         question: "What are variables used for in JavaScript Programs?",
         number: 1,
-        choices: {
-            a: "Storing numbers, dates, or other values",
-            b: "Causing high-school algebra flashbacks",
-            c: "Varying randomly",
-            d: "None of the above"
-        },
-        answer: "a"
+        choices: [
+            "Storing numbers, dates, or other values",
+            "Causing high-school algebra flashbacks",
+            "Varying randomly",
+            "None of the above"
+        ],
+        answer: "Storing numbers, dates, or other values"
     },
     {
         question: "What should appear at the very end of your JavaScript?",
         number: 2,
-        choices: {
-            a: "The END statement",
-            b: "The closing meta tag",
-            c: "The closing script tag",
-            d: "None of the above"
-        },
-        answer: "c"
+        choices: [
+            "The END statement",
+            "The closing meta tag",
+            "The closing script tag",
+            "None of the above"
+        ],
+        answer: "The closing script tag"
     },
     {
         question: "What are Javascript functions capable of performing?",
         number: 3,
-        choices: {
-            a: "return values",
-            b: "accept parameters",
-            c: "Both",
-            d: "Neither"
-        },
-        answer: "b"
+        choices: [
+            "return values",
+            "accept parameters",
+            "Both",
+            "Neither",
+        ],
+        answer: "accept parameters"
     },
     {
         question: "Which of the following is NOT a vaild JavaScript variable name?",
         number: 4,
-        choices: {
-            a: "pi",
-            b: "produceArray",
-            c: "generate_string",
-            d: "5listof2dogs"
-        },
-        answer: "d"
+        choices: [
+            "pi",
+            "produceArray",
+            "generate_string",
+            "5listof2dogs"
+        ],
+        answer: "5listof2dogs"
     },
     {
         question: "Which tag is an HTML extension that can enclose any number of JavaScript statemnts?",
         number: 5,
-        choices: {
-            a: "header",
-            b: "script",
-            c: "footer",
-            d: "title"
-        },
-        answer: "b"
+        choices: [
+            "header",
+            "script",
+            "footer",
+            "title"
+        ],
+        answer: "script"
     },
     {
         question: "What is the correct syntax to write 'My Pet Dog' in JavaScript?",
         number: 6,
-        choices: {
-            a: "response.write(\"My Pet Dog\")",
-            b: "printIn(\"My Pet Dog\")",
-            c: "System.out.printIn(\"My Pet Dog\")",
-            d: "document.write(\"My Pet Dog\")"
-        },
-        answer: "d"
+        choices: [
+            "response.write(\"My Pet Dog\")",
+            "printIn(\"My Pet Dog\")",
+            "System.out.printIn(\"My Pet Dog\")",
+            "document.write(\"My Pet Dog\")"
+        ],
+        answer: "document.write(\"My Pet Dog\")"
     },
     {
         question: "In the HTML, where is the JavaScript correctly placed?",
         number: 7,
-        choices: {
-            a: "head",
-            b: "javascript",
-            c: "script",
-            d: "js"
-        },
-        answer: "c"
+        choices: [
+            "head",
+            "javascript",
+            "script",
+            "js"
+        ],
+        answer: "script"
     },
     {
         question: "The correct syntax for linking a .html to a .js file contains ___________?",
         number: 8,
-        choices: {
-            a: "src",
-            b: "href",
-            c: "rel",
-            d: "id"
-        },
-        answer: "a"
+        choices: [
+            "src",
+            "href",
+            "rel",
+            "id"
+        ],
+        answer: "src"
     },
     {
         question: "Which is the server-side JavaScript object?",
         number: 9,
-        choices: {
-            a: "FileUpLoad",
-            b: "function",
-            c: "date",
-            d: "file"
-        },
-        answer: "d"
+        choices: [
+            "FileUpLoad",
+            "function",
+            "date",
+            "file"
+        ],
+        answer: "file"
     },
     {
         question: "How are specific conditions tested for in JavaScript? Which statement is utilized?",
         number: 10,
-        choices: {
-            a: "If",
-            b: "For",
-            c: "Select",
-            d: "Switch"
-        },
-        answer: "a"
+        choices: [
+            "If",
+            "For",
+            "Select",
+            "Switch"
+        ],
+        answer: "If"
     }
 ];
 
-var startBtn = document.getElementById("start");
-var infoBox = document.querySelector(".info_box");
-var continueBtn = info_box.querySelector(".buttons .restart");
 
-// if startQuiz button clicked
-start_btn.onclick = ()=>{
-    infoBox.classList.add("activeInfo"); //show info box
-}
-
-continue_btn.onclick = ()=>{
-    infoBox.classList.remove("activeInfo"); //hide info box
-    quizBox.classList.add("activeQuiz"); //show quiz box
-    showQuetions(0); //calling showQestions function
-    queCounter(1); //passing 1 parameter to queCounter
-    startTimer(100); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
-}
-
-let timeValue =  15;
-let que_count = 0;
-let que_numb = 1;
-let userScore = 0;
-let counter;
-let counterLine;
-let widthValue = 0;
-
-var nextButton = document.querySelector("footer .next_btn");
-var bottomQueCounter = document.querySelector("footer .total_que");
-
-// if Next Que button clicked
-nextButton.onclick = ()=>{
-    if(que_count < questions.length - 1) { //if question count is less than total question length
-        que_count++; //increment the que_count value
-        que_numb++; //increment the que_numb value
-        showQuetions(que_count); //calling showQestions function
-        queCounter(que_numb); //passing que_numb value to queCounter
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
-        startTimer(timeValue); //calling startTimer function
-        startTimerLine(widthValue); //calling startTimerLine function
-        timeText.textContent = "Time Left"; //change the timeText to Time Left
-        next_btn.classList.remove("show"); //hide the next button
-    }
-    else {
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
-        showResult(); //calling showResult function
-    }
-}
-
-// getting questions and options from array
-function showQuetions(index){
-    const que_text = document.querySelector("quiz-questions");
-    //creating a new span and div tag for question and option and passing the value using array index
-    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
-    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
-    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
-    
-    const option = option_list.querySelectorAll(".option");
-    // set onclick attribute to all available options
-    for(i=0; i < option.length; i++){
-        option[i].setAttribute("onclick", "optionSelected(this)");
-    }
-}
-
-//if user clicked on option
-function optionSelected(answer){
-    clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
-    let userAns = answer.textContent; //getting user selected option
-    let correcAns = questions[que_count].answer; //getting correct answer from array
-    const allOptions = option_list.children.length; //getting all option items
-    
-    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
-        userScore += 1; //upgrading score value with 1
-        answer.classList.add("correct"); //adding green color to correct selected option
-        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
-        console.log("Correct Answer");
-        console.log("Your correct answers = " + userScore);
-    }else{
-        answer.classList.add("incorrect"); //adding red color to correct selected option
-        answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
-        console.log("Wrong Answer");
-        for(i=0; i < allOptions; i++){
-            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
-                option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                console.log("Auto selected correct answer.");
-            }
-        }
-    }
-    for(i=0; i < allOptions; i++){
-        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
-    }
-    next_btn.classList.add("show"); //show the next button if user selected any option
-}
-
-function showResult(){
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.remove("activeQuiz"); //hide quiz box
-    result_box.classList.add("activeResult"); //show result box
-    const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
-        //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
-    }
-    else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-    else{ // if user scored less than 1
-        let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-}
-function startTimer(time){
-    counter = setInterval(timer, 1000);
-    function timer(){
-        timeCount.textContent = time; //changing the value of timeCount with time value
-        time--; //decrement the time value
-        if(time < 9){ //if timer is less than 9
-            let addZero = timeCount.textContent; 
-            timeCount.textContent = "0" + addZero; //add a 0 before time value
-        }
-        if(time < 0){ //if timer is less than 0
-            clearInterval(counter); //clear counter
-            timeText.textContent = "Time Off"; //change the time text to time off
-            const allOptions = option_list.children.length; //getting all option items
-            let correcAns = questions[que_count].answer; //getting correct answer from array
-            for(i=0; i < allOptions; i++){
-                if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
-                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                    console.log("Time Off: Auto selected correct answer.");
-                }
-            }
-            for(i=0; i < allOptions; i++){
-                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
-            }
-            next_btn.classList.add("show"); //show the next button if user selected any option
-        }
-    }
-}
-function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
-    function timer(){
-        time += 1; //upgrading time value with 1
-        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
-            clearInterval(counterLine); //clear counterLine
-        }
-    }
-}
-
-function queCounter(index){
-    //creating a new span tag and passing the question number and total question
-    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
-    bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
-}
