b03422b0164c65c4c438822d2aad7749d3ef5447
Coder J <buildingguy@sbcglobal.net>
2021-12-11 14:15:25 -0800

    set up js questions and html

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var questions = [ "What are variables used for in JavaScript Programs?",
+"What should appear at the very end of your JavaScript?",
+"What are Javascript functions capable of performing?",
+"Which of the following is NOT a vaild JavaScript variable name?",
+"Which tag is an HTML extension that can enclose any number of JavaScript statemnts?",
+"What is the correct syntax to write 'My Pet Dog' in JavaScript?",
+"In the HTML, where is the JavaScript correctly placed?",
+"The correct syntax for linking a .html to a .js file contains ___________?",
+"Which of the following is NOT a vaild JavaScript variable name?",
+"Which is the server-side JavaScript object?",
+"How are specific conditions tested for in JavaScript? Which statement is utilized?"
+];
